95ad3653fb3594856c4db162fa4ab4d1e47fb44b
Lena <Lenala@gmail.com>
2020-05-07 14:12:04 +0200

    our implementation

---
+++ Implementation/collision.py
@@ -0,0 +1,107 @@
+from __future__ import division
+import math
+
+
+class Collision():
+    def __init__(self):
+        pass
+
+    def check_for_collisions(self, next_step_x, next_step_y, last_step_x, last_step_y, indexer):
+        """
+        Checks whether the computed next step will create a collision
+        :param: next_step_x
+        :param: next_step_y
+        :param: last_step_x
+        :param: last_step_y
+        :param: indexer = required indexer object for grid computations
+        :returns: bool for collision or not
+        """
+        # get frames that cross in the next step that should be taken
+        grid_frames = indexer.get_frames_at_xy(next_step_x, next_step_y)
+
+        cell_tracks = {}
+        # iterate over all frames in Grid-Cell
+        for frame in grid_frames:
+            if not (frame.pedestrian in cell_tracks.keys()):
+                # get all frames of the pedestrian (not just in grid cell)
+                pedestrian_frames = indexer.get_all_frames_by_ped_id(frame.pedestrian)
+                # filter so that only one frame before or after or the same frame remain in list
+                try:
+                    pedestrian_frames = [fr for fr in pedestrian_frames if (
+                            (fr.frame == frame.frame + 1) or (fr.frame == frame.frame - 1) or (
+                            fr.frame == frame.frame))]
+                except IndexError:
+                    pass
+                cell_tracks[frame.pedestrian] = indexer.get_ped_traj_xy(pedestrian_frames)
+
+        # iterate over the paths of the pedestrians in the grid cell
+        for ped, track in cell_tracks.items():
+            new_line = ([next_step_x, next_step_y], [last_step_x, last_step_y])
+
+            # create line that crosses in the cell
+            line2 = (track[1], track[0])
+            collide1, intersection_point = self.line_intersection(line2, new_line)
+
+            # if we have an incoming frame and an outgoing from from grid cell
+            collide2 = None
+            if len(track) == 3:
+                line3 = (track[2], track[1])
+                collide2, intersection_point = self.line_intersection(line3, new_line)
+
+            if collide1 or collide2:
+                return True
+            else:
+                continue
+
+        return False
+
+    def line_intersection(self, existing_line, new_line):
+        """"
+        Computes the intersection between two lines
+        :param: existing line = line from database in grid cell
+        :param: new_line = newly computed line
+        :returns: True, (intersection_x, intersection_y) if intersection
+        :returns: False, (None, None) if no intersection
+        """
+        # get differences in x- and y-values
+        xdiff = (existing_line[0][0] - existing_line[1][0], new_line[0][0] - new_line[1][0])
+        ydiff = (existing_line[0][1] - existing_line[1][1], new_line[0][1] - new_line[1][1])
+
+        # determinant of a, b
+        def det(a, b):
+            return a[0] * b[1] - a[1] * b[0]
+
+        # if determinant 0: no intersection
+        div = det(xdiff, ydiff)
+        if div == 0:
+            return False, (None, None)
+
+        # else: compute intersection point
+        d = (det(*existing_line), det(*new_line))
+        x = det(d, xdiff) / div
+        y = det(d, ydiff) / div
+        intersection_point = (x, y)
+
+        # compute if collision/intersection is on new segment of line
+        collide = self.intersection_in_new_segment(existing_line, intersection_point)
+        # only if collision on new segment, return true
+        if collide:
+            return True, intersection_point
+        else:
+            return False, (None, None)
+
+    def intersection_in_new_segment(self, new_line, intersection_point):
+        """
+        Computes whether intersection in current grid cell
+        returns: bool
+        """
+        def distance(point_1, point_2):
+            return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
+
+        point_1 = new_line[0]
+        point_2 = new_line[1]
+        # if intersection_point is between the endpoints of the segment of new_line
+        # then the distance between (start_new to intersection) + (intersection to end_new) == (start_new, end_new)
+        return distance(point_1, intersection_point) + distance(point_2, intersection_point) == distance(point_1, point_2)
+
+
---
+++ Implementation/predict_from_db_and_mean_mvmt.py
@@ -0,0 +1,345 @@
+from Implementation import collision
+
+import math
+import json
+import argparse
+from collections import defaultdict, namedtuple
+import numpy as np
+import copy
+from statistics import mean
+
+
+TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'prediction_number', 'scene_id'])
+TrackRow.__new__.__defaults__ = (None, None, None, None, None, None)
+SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end', 'fps', 'tag'])
+GridPoint = namedtuple('Point', ['frame', 'pedestrian', 'x', 'y', 'pre_x', 'pre_y',
+                                 'next_x', 'next_y', 'prediction_number', 'scene_id'])
+GridPoint.__new__.__defaults__ = (None, None, None, None, None, None, None, None, None, None)
+
+
+class Indexer(object):
+    """Read trajnet files and put tracks into grid
+
+    :param grid_dim: defines grid dimension  n x n
+    """
+
+    def __init__(self, input_file, grid_dim=100):
+
+        self.grid_dim = grid_dim
+        self.grid = [[[] for x in range(self.grid_dim)] for y in range(self.grid_dim)]
+
+        self.tracks = []
+        self.scenes_by_frame = defaultdict(list)
+
+        self.x_max = self.y_max = float('-inf')
+        self.x_min = self.y_min = float('inf')
+
+        self.read_file(input_file)
+
+        self.y_dim = abs(self.y_max) + abs(self.y_min)
+        self.x_dim = abs(self.x_max) + abs(self.x_min)
+
+        self.fill_grid()
+
+    # Reads file and extract environment dimension
+    # creates tracks and scene lists
+    def read_file(self, input_file):
+        self.scene_frames_start_end = {}  # scene_id:[frame_start, frame_end]
+        with open(input_file, 'r') as f:
+            for line in f:
+                line = json.loads(line)
+
+                track = line.get('track')
+                if track is not None:
+                    row = TrackRow(track['f'], track['p'], track['x'], track['y'], \
+                                   track.get('prediction_number'), track.get('scene_id'))
+
+                    self.x_max = row.x if row.x > self.x_max else self.x_max
+                    self.x_min = row.x if row.x < self.x_min else self.x_min
+                    self.y_max = row.y if row.y > self.y_max else self.y_max
+                    self.y_min = row.y if row.y < self.y_min else self.y_min
+
+                    self.tracks.append(row)
+                    continue
+
+                scene = line.get('scene')
+                if scene is not None:
+                    self.scene_frames_start_end[scene['id']]=[scene['s'], scene['e']]
+                    row = SceneRow(scene['id'], scene['p'], scene['s'], scene['e'], \
+                                   scene['fps'], scene['tag'])
+                    for i in range(row.start, row.end + 1):
+                        self.scenes_by_frame[i].append(row.scene)
+
+        self.x_max = math.ceil(self.x_max)
+        self.x_min = math.floor(self.x_min)
+        self.y_max = math.ceil(self.y_max)
+        self.y_min = math.floor(self.y_min)
+
+    # fills grid with the tracks
+    def fill_grid(self):
+        for i in range(len(self.tracks)):
+            frame = self.tracks[i].frame
+            pedestrian = self.tracks[i].pedestrian
+            x = self.tracks[i].x
+            y = self.tracks[i].y
+            prediction_number = self.tracks[i].prediction_number
+            scene_id = self.tracks[i].scene_id
+            pre_x = pre_y = next_x = next_y = None
+
+            # If a previous point on the track of this point exists, save the coordinates
+            if (self.tracks[i - 1].frame + 1) == frame \
+                    and self.tracks[i - 1].pedestrian == pedestrian:
+                pre_x = self.tracks[i - 1].x
+                pre_y = self.tracks[i - 1].y
+
+            # If a next point on the track of this point exists, save the coordinates
+            if i < len(self.tracks) - 1:
+                if (self.tracks[i + 1].frame - 1) == frame \
+                        and self.tracks[i + 1].pedestrian == pedestrian:
+                    next_x = self.tracks[i + 1].x
+                    next_y = self.tracks[i + 1].y
+
+            point = GridPoint(frame, pedestrian, x, y, pre_x, pre_y, next_x, next_y, prediction_number, scene_id)
+            self.grid[self.convert_x(point.x)][self.convert_y(point.y)].append(point)
+
+    # returns x-coordinate inside the grid
+    def convert_x(self, x):
+        tmp_x = x + abs(self.x_min)
+        return math.floor((tmp_x / self.x_dim) * self.grid_dim)
+
+    # returns y-coordinate inside the grid
+    def convert_y(self, y):
+        tmp_y = y + abs(self.y_min)
+        return math.floor((tmp_y / self.y_dim) * self.grid_dim)
+
+    # returns all scene ids from a certain frame
+    def get_scenes_at_frame(self, frame):
+        return self.scenes_by_frame[frame]
+
+    # returns whole grid
+    def get_grid(self):
+        return self.grid
+
+    # returns all frames from a grid panel which includes the coordinates x,y
+    def get_frames_at_xy(self, x, y):
+        return self.grid[self.convert_x(x)][self.convert_y(y)]
+
+    def get_all_frames_by_ped_id(self, ped_id):
+        all_ped_frames = []
+        for i in range(len(self.tracks)):
+            if self.tracks[i].pedestrian == ped_id:
+                all_ped_frames.append(self.tracks[i])
+        return all_ped_frames
+
+    def get_ped_traj(self, all_ped_frames):
+        xy = []
+        ped_traj = []
+        for i, p in enumerate(all_ped_frames):
+            if [p.x, p.y] not in xy:
+                xy.append([p.x, p.y])
+                ped_traj.append(p)
+                print(i, p)
+        return ped_traj
+
+    def get_ped_traj_xy(self, all_ped_frames):
+        xy = []
+        ped_traj = []
+        for i, p in enumerate(all_ped_frames):
+            if [p.x, p.y] not in xy:
+                xy.append([p.x, p.y])
+                ped_traj.append(p)
+                print(i, p)
+        return xy
+
+    def get_all_frames_by_scene_id(self, scene_id):
+        all_frames_by_scene_id = []
+        unique_pedestrians = []
+        for i in range(len(self.tracks)):
+            if self.tracks[i].frame >= self.scene_frames_start_end[scene_id][0] and self.tracks[i].frame <= self.scene_frames_start_end[scene_id][1]:
+                all_frames_by_scene_id.append(self.tracks[i])
+                unique_pedestrians.append(self.tracks[i].pedestrian)
+        unique_pedestrians = list(set(unique_pedestrians))
+        return all_frames_by_scene_id, unique_pedestrians
+
+    def get_scene_prediction_datastracture(self, all_frames_by_scene_id):
+        scene_prediction_datastracture = {}
+        # iterate over all frames to group pedestrians together
+        for f in all_frames_by_scene_id:
+            if f.pedestrian not in scene_prediction_datastracture.keys():
+                scene_prediction_datastracture[f.pedestrian] = {}
+                scene_prediction_datastracture[f.pedestrian][f.frame] = f
+            else:
+                scene_prediction_datastracture[f.pedestrian][f.frame] = f
+        # iterate over pedestrians
+        for p in scene_prediction_datastracture.keys():
+            all_ped_frames = self.get_all_frames_by_ped_id(p)
+            ped_traj = self.get_ped_traj(all_ped_frames)
+            all_per_frames_in_scene = list(scene_prediction_datastracture[p].keys())
+
+            common_frame_id = list(np.intersect1d([ff.frame for ff in all_ped_frames], all_per_frames_in_scene))
+            common_frame_id = common_frame_id[0]
+            for f in all_frames_by_scene_id:  # find common frame in the scene data
+                if f.frame == common_frame_id and p == f.pedestrian:
+                    x = f.x  # scene data
+                    y = f.y  # scene data
+                    fr = f.frame  # scene data
+            for i, f in enumerate(ped_traj):  # find common frame in the ped traj data
+                if f.x == x and f.y == y:
+                    index_frame = fr-i
+            for i in range(len(ped_traj)):
+                ped_traj[i] = copy.deepcopy(ped_traj[i]._replace(frame=index_frame))
+                index_frame += 1
+            for i, f in enumerate(ped_traj):
+                if f.frame in list(scene_prediction_datastracture[p].keys()):
+                    if f.x == scene_prediction_datastracture[p][f.frame].x and f.y == scene_prediction_datastracture[p][f.frame].y:
+                        pass
+                    else:
+                        print('error')
+                else:
+                    scene_prediction_datastracture[p][f.frame] = f
+        return scene_prediction_datastracture
+
+    def predict_mean_movement(self, line_ped_x, line_ped_y, line_ped_x_mean, line_ped_y_mean, frame_id, ped_id, scene_id):
+        """
+        Computes the average movement from the previous steps and predicts one more step based on that
+        returns: newly computed lines (with appended step if no collision)
+        """
+        mvmt_x = []
+        mvmt_y = []
+
+        # compute average movement for each step/frame
+        for j in range(len(line_ped_x) - 1):
+            mvmt_x.append(line_ped_x[j + 1] - line_ped_x[j])
+            mvmt_y.append(line_ped_y[j + 1] - line_ped_y[j])
+
+        # store last step for grid update
+        last_step_x = line_ped_x[-1]
+        last_step_y = line_ped_y[-1]
+
+        # next step is the last step + mean of all movement-values
+        next_step_x = last_step_x + mean(mvmt_x)
+        next_step_y = last_step_y + mean(mvmt_y)
+
+        # check for collisions when creating new path
+        col_object = collision.Collision()
+        line_collision = col_object.check_for_collisions(next_step_x, next_step_y, last_step_x, last_step_y,
+                                                         self)
+
+        if not line_collision:
+            # extend line to be plotted with this
+            line_ped_x.append(next_step_x)
+            line_ped_y.append(next_step_y)
+            # additional line for plotting only the extension by mean
+            line_ped_x_mean.append(next_step_x)
+            line_ped_y_mean.append(next_step_y)
+            # extend the grid with the newest step
+            new_track = TrackRow(frame_id, ped_id, next_step_x, next_step_y, None, scene_id)
+            self.extend_grid(track=new_track, pre_x=last_step_x, pre_y=last_step_y)
+        else:
+            raise Exception("Intersection Found")
+
+        return line_ped_x, line_ped_y, line_ped_x_mean, line_ped_y_mean
+
+    def extend_grid(self, track, pre_x, pre_y):
+        """
+        Extend the grid by the TrackRow track
+        """
+        next_x = next_y = None
+        point = GridPoint(track.frame, track.pedestrian, track.x, track.y, pre_x, pre_y, next_x, next_y,
+                          track.prediction_number, track.scene_id)
+        self.grid[self.convert_x(point.x)][self.convert_y(point.y)].append(point)
+
+    def backtracking(self, line_ped_x, line_ped_y, line_ped_x_in, line_ped_y_in):
+        computed_steps = len(line_ped_x) - len(line_ped_x_in)
+
+        if computed_steps == 0:
+            return None
+
+        backtracking_steps = computed_steps if computed_steps <= 5 else 5
+
+        backtracking_line_x = line_ped_x[0:len(line_ped_x)-backtracking_steps]
+        backtracking_line_y = line_ped_y[0:len(line_ped_y)-backtracking_steps]
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ndjson', help='trajnet dataset file')
+    parser.add_argument('--n', type=int, default=100, help='grid dimension n x n')
+    parser.add_argument('--scene_id', type=int, default=2396, help="scene you want to look at")
+    args = parser.parse_args()
+
+    print(args.ndjson)
+    indexer = Indexer(args.ndjson, args.n)
+    grid = indexer.get_grid()
+    print([indexer.x_max, indexer.x_min, indexer.y_max, indexer.y_min])
+    for p in indexer.get_frames_at_xy(indexer.tracks[5].x, indexer.tracks[5].y):
+        print(p)
+
+    scene_id = args.scene_id
+    all_frames_by_scene_id, unique_pedestrians = indexer.get_all_frames_by_scene_id(scene_id)
+
+    scene_prediction_datastracture = indexer.get_scene_prediction_datastracture(all_frames_by_scene_id)
+
+    # scene_ped_curves = {}
+    # for ped in unique_pedestrians:
+    #     all_ped_frames = indexer.get_all_frames_by_ped_id(ped)
+    #     ped_traj = indexer.get_ped_traj(all_ped_frames, ped)
+    #     scene_ped_curves[ped] = ped_traj
+    # for i in scene_ped_curves.keys():
+    #     ax = plt.plot([p.x for p in scene_ped_curves[i]], [p.y for p in scene_ped_curves[i]], '*')
+
+    # matplotlib.use('MacOSX')
+    import matplotlib.pyplot as plt
+
+    # for ped in range(10):
+    #     all_ped_frames = indexer.get_all_frames_by_ped_id(ped)
+    #     ped_traj = indexer.get_ped_traj(all_ped_frames, ped)
+    #     ax = plt.plot([p.x for p in all_ped_frames], [p.y for p in all_ped_frames], '*')
+    for p in scene_prediction_datastracture.keys():
+        line_ped_x = []
+        line_ped_y = []
+        line_ped_x_in = []
+        line_ped_y_in = []
+        line_ped_x_mean = []
+        line_ped_y_mean = []
+
+        # iterate over all frames that the scene has
+        for i in range(indexer.scene_frames_start_end[scene_id][0], indexer.scene_frames_start_end[scene_id][1]):
+            try:
+                # append the values found in the database
+                line_ped_x.append(scene_prediction_datastracture[p][i].x)
+                line_ped_y.append(scene_prediction_datastracture[p][i].y)
+                # append the input values?
+                if i <= 9 + indexer.scene_frames_start_end[scene_id][0]:
+                    line_ped_x_in.append(scene_prediction_datastracture[p][i].x)
+                    line_ped_y_in.append(scene_prediction_datastracture[p][i].y)
+            except:  # no database entries found
+                try:
+                    # try to predict the movement by taking mean movement of pedestrian before
+                    line_ped_x, line_ped_y, line_ped_x_mean, line_ped_y_mean = indexer.predict_mean_movement(line_ped_x,
+                                                                                                             line_ped_y,
+                                                                                                             line_ped_x_mean,
+                                                                                                             line_ped_y_mean,
+                                                                                                             ped_id=p,
+                                                                                                             scene_id=scene_id,
+                                                                                                             frame_id=i)
+                except Exception:
+                    indexer.backtracking(line_ped_x, line_ped_y, line_ped_x_in, line_ped_y_in)
+        ax = plt.plot(line_ped_x, line_ped_y)
+        ax = plt.plot(line_ped_x_in, line_ped_y_in, linewidth=3, linestyle=":")
+        ax = plt.plot(line_ped_x_mean, line_ped_y_mean, linewidth=3, linestyle="--")
+
+    ax[0].axes.set_aspect('equal')
+    plt.title([args.ndjson.split('/')[-1], "scene_id", scene_id])
+    plt.show()
+
+    print('DONE')
+    # collect scene frames
+    # collect ped trajectories
+    # assign start in ped trajectories
+    # extend scene trajectories
+
+
+if __name__ == '__main__':
+    main()
